35e2993ea56dbe2b9618d69aea4a03eb6eb25d38
Saad Khan <s.aad@live.com>
2018-10-17 12:30:09 +0500

    Instead of inserting span as html, span tag pasted from background page.

--- background.js
+++ background.js
@@ -1,6 +1,23 @@
 chrome.commands.onCommand.addListener(function(command) {
-  if(command == "insert-span")
+  if(command == "insert-span"){
+    selectText("sandbox");
+    document.execCommand("copy");
     chrome.tabs.executeScript({
           file: 'contentScript.js'
         });
+  }
 });
+
+// gist source: https://stackoverflow.com/questions/1173194/select-all-div-text-with-single-mouse-click
+function selectText(containerid) {
+    if (document.selection) { // IE
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(containerid));
+        range.select();
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(document.getElementById(containerid));
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+    }
+}
